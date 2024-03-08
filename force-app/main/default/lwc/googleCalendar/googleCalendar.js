23add293f06f1de534c07e712f520779a9ee8e65
Soham Datta <55832394+iamSoham01@users.noreply.github.com>
2024-03-09 01:57:06 +0530

    Updated Google Calendar

---
+++ force-app/main/default/lwc/googleCalendar/googleCalendar.js
@@ -0,0 +1,104 @@
+import { LightningElement, track } from 'lwc';
+import createCalenderMeeting from '@salesforce/apex/GoogleUtil.createCalenderMeeting';
+
+export default class GoogleCalendar extends LightningElement {
+  
+  timezones = [];
+  error;
+  @track meetingDetails;
+
+  connectedCallback() {
+    this.loadTimeZoneData();
+  }
+
+  async loadTimeZoneData() {
+    try {
+        // Load the time zone data from the World Time API
+        const response = await fetch('https://worldtimeapi.org/api/timezone');
+        const timezones = await response.json();
+
+        // Populate the combobox options
+        this.timezones = timezones.map(timezone => ({
+            label: timezone,
+            value: timezone
+        }));
+    } catch(error) {
+        console.error('Error fetching time zone data:', error);
+    }
+  }
+
+  isValidEmail(email) {
+    // Email validation regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  checkFieldValidity(inputType, errorMsg) {
+
+    if (!inputType.value) {
+      inputType.setCustomValidity(`Please enter ${errorMsg}`);
+    } else {
+      inputType.setCustomValidity(``);
+    }
+    return inputType.reportValidity();
+  }     
+
+  handleCreateMeeting() {
+
+    const startDate = this.refs.startDate;
+    const startTime = this.refs.startTime;
+    const endDate = this.refs.endDate;
+    const endTime = this.refs.endTime;
+    const timeZone = this.refs.timeZone;
+    const meetingSubject = this.refs.meetingSubject;
+    const meetingDescription = this.refs.meetingDescription;
+    const attendeesEmail = this.refs.attendeesEmail;
+
+    const checkStartDateValid = this.checkFieldValidity(startDate, `start date`);
+    const checkStartTimeValid = this.checkFieldValidity(startTime, `start time`);
+    const checkEndDateValid = this.checkFieldValidity(endDate, `end date`);
+    const checkEndTimeValid = this.checkFieldValidity(endTime, `end time`);
+    const checkTimeZoneValid = this.checkFieldValidity(timeZone, `time zone`);
+    const checkMeetingSubjectValid =this.checkFieldValidity(meetingSubject, `meeting subject`);
+    const checkAttendeeEmailsValid = this.checkFieldValidity(attendeesEmail, `attendees email address`);
+
+    if(!checkStartDateValid || !checkStartTimeValid || !checkEndDateValid || !checkEndTimeValid || !checkTimeZoneValid || !checkMeetingSubjectValid || !checkAttendeeEmailsValid){
+      return;
+    }
+
+    const attendeesEmailList = attendeesEmail.value.split('\n').filter(email => email.trim() !== '').filter(email => this.isValidEmail(email));
+    const startDateTime = `${startDate.value}T${startTime.value.substring(0, 8)}`;
+    const endDateTime = `${endDate.value}T${endTime.value.substring(0, 8)}`;
+
+    let mapAttributes = {
+      endDateTime: endDateTime,
+      startDateTime: startDateTime,
+      timeZone: timeZone.value,
+      meetingSubject: meetingSubject.value,
+      meetingDescription: meetingDescription.value,
+      attendeesEmailList: attendeesEmailList
+    };
+
+    createCalenderMeeting({mapAttributes: mapAttributes})
+      .then((result) => {
+        console.log(`result: `,JSON.stringify(result));
+        this.meetingDetails = result;
+        console.log(`this.meetingDetails: `,JSON.stringify(this.meetingDetails));
+        this.error = ``;
+        startDate.value = ``;
+        startTime.value = ``;
+        endDate.value = ``;
+        endTime.value = ``;
+        meetingSubject.value = ``;
+        meetingDescription.value = ``;
+        attendeesEmail.value = ``;
+      })
+      .catch((error) => {
+        this.error = error.message;
+        this.meetingDetails = ``;
+      });
+    
+
+  }
+
+}
--- force-app/main/default/lwc/googleCallout/googleCallout.js
+++ force-app/main/default/lwc/googleCallout/googleCallout.js
@@ -5,8 +5,6 @@ import GMAIL_LOGO from '@salesforce/resourceUrl/gmail';
 import DRIVE_LOGO from '@salesforce/resourceUrl/googleDrive';
 import MAPS_LOGO from '@salesforce/resourceUrl/googleMaps';
 
-import createCalenderMeeting from '@salesforce/apex/GoogleUtil.createCalenderMeeting';
-
 export default class GoogleCallout extends LightningElement {
 
   resources = {
@@ -17,145 +15,16 @@ export default class GoogleCallout extends LightningElement {
     mapsLogo: MAPS_LOGO
   };
 
-  googleLogo = GOOGLE_LOGO;
-  timezones = [];
-  error;
-  meetingURL;
+  isCalendar = true;
+  isGmail = false;
+  isMaps = false;
+  isDrive = false;
 
-  connectedCallback() {
-    this.loadTimeZoneData();
-  }
-
-  async loadTimeZoneData() {
-    try {
-        // Load the time zone data from the World Time API
-        const response = await fetch('https://worldtimeapi.org/api/timezone');
-        const timezones = await response.json();
-
-        // Populate the combobox options
-        this.timezones = timezones.map(timezone => ({
-            label: timezone,
-            value: timezone
-        }));
-    } catch(error) {
-        console.error('Error fetching time zone data:', error);
-    }
-  }
-
-  isValidEmail(email) {
-    // Email validation regex
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
-
-  // handleAuthentication() {
-  //   window.open('/apex/'+ googleAuthenticationPage, '_blank');
-  // }
-
-  checkFieldValidity(inputType, errorMsg) {
-
-    if (!inputType.value) {
-      inputType.setCustomValidity(`Please enter ${errorMsg}`);
-    } else {
-      inputType.setCustomValidity(``);
-    }
-    return inputType.reportValidity();
-  }
-
-  attendeesJSON(emails) {
-
-    let jsonStrings = [];
-    // Iterate over each email address
-    emails.forEach(email => {
-      // Construct a JSON object with the current email address
-      let jsonObject = {
-          email: email
-      };
-      // Convert the JSON object to a string and push it to the array
-      jsonStrings.push(JSON.stringify(jsonObject));
-    });
-    return jsonStrings;
-  }            
-
-  handleCreateMeeting() {
-
-    const senderEmail = this.refs.senderEmail;
-    const startDate = this.refs.startDate;
-    const startTime = this.refs.startTime;
-    const endDate = this.refs.endDate;
-    const endTime = this.refs.endTime;
-    const timeZone = this.refs.timeZone;
-    const meetingSubject = this.refs.meetingSubject;
-    const meetingDescription = this.refs.meetingDescription;
-    const attendeesEmail = this.refs.attendeesEmail;
-
-    const checkSenderEmailValid = this.checkFieldValidity(senderEmail, `sender email`);
-    const checkStartDateValid = this.checkFieldValidity(startDate, `start date`);
-    const checkStartTimeValid = this.checkFieldValidity(startTime, `start time`);
-    const checkEndDateValid = this.checkFieldValidity(endDate, `end date`);
-    const checkEndTimeValid = this.checkFieldValidity(endTime, `end time`);
-    const checkTimeZoneValid = this.checkFieldValidity(timeZone, `time zone`);
-    const checkMeetingSubjectValid =this.checkFieldValidity(meetingSubject, `meeting subject`);
-    const checkAttendeeEmailsValid = this.checkFieldValidity(attendeesEmail, `attendees email address`);
-
-    if(!checkSenderEmailValid || !checkStartDateValid || !checkStartTimeValid || !checkEndDateValid || !checkEndTimeValid || !checkTimeZoneValid || !checkMeetingSubjectValid || !checkAttendeeEmailsValid){
-      return;
-    }
-
-    const attendeesEmailList = attendeesEmail.value.split('\n').filter(email => email.trim() !== '').filter(email => this.isValidEmail(email));
-    const startDateTime = `${startDate.value}T${startTime.value.substring(0, 8)}`;
-    const endDateTime = `${endDate.value}T${endTime.value.substring(0, 8)}`;
-
-    let requestBody = '{'
-                        +'"end": {'
-                        +  '"dateTime": "' + endDateTime + '",'
-                        +  '"timeZone": "' + timeZone.value + '"'
-                        + '},'
-                        + '"start": {'
-                        + '"dateTime": "' + startDateTime + '",'
-                        + '"timeZone": "' + timeZone.value + '"'
-                        + '},'
-                        + '"summary": "' + meetingSubject.value + '",'
-                        + '"location": "Online",'
-                        + '"sendUpdates": "all",'
-                        + '"reminders": {'
-                        +  '"useDefault": false,'
-                        +  '"overrides": ['
-                        +    '{'
-                        +      '"method": "email",'
-                        +      '"minutes": 1440'
-                        +    '},'
-                        +    '{'
-                        +      '"method": "popup",'
-                        +      '"minutes": 60'
-                        +    '}'
-                        +  ']'
-                        + '},'
-                        + '"description": "' + meetingDescription.value + '",'
-                        + '"attendees":' + '[' 
-                        + this.attendeesJSON(attendeesEmailList) 
-                        + ']'
-                      +'}';
-
-    createCalenderMeeting({ calendarId: senderEmail.value, requestBody: requestBody})
-      .then((result) => {
-        this.meetingURL = result;
-        this.error = ``;
-        senderEmail.value = ``;
-        startDate.value = ``;
-        startTime.value = ``;
-        endDate.value = ``;
-        endTime.value = ``;
-        meetingSubject.value = ``;
-        meetingDescription.value = ``;
-        attendeesEmail.value = ``;
-      })
-      .catch((error) => {
-        this.error = error.message;
-        this.meetingURL = ``;
-      });
-    
-
+  handleMenuBtn(event){
+    this.isCalendar = event.target.dataset.name === 'Calendar';
+    this.isGmail = event.target.dataset.name === 'Gmail';
+    this.isMaps = event.target.dataset.name === 'Maps';
+    this.isDrive = event.target.dataset.name === 'Drive';
   }
 
 }
